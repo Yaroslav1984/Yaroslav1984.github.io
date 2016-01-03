d1e7767b325e958a8fe7b7965c725941f8fa3d87
Yaroslav Zamorskyy <zamorsk20@gmail.com>
2016-01-03 12:39:08 +0200

    JS3-4

--- GoIT/markup/home/JavaScript work 3-4/js/script3-4.js
+++ GoIT/markup/home/JavaScript work 3-4/js/script3-4.js
@@ -1,27 +1,45 @@
 var app = {
-
 	createElement: function(params) {
       var element = document.createElement(params.tagName);
-
-      console.log("params.inputType", params.inputType);
-
+ 
       if (params.inputType){
       	element.setAttribute('type', params.inputType);
-      } else {
-      	element.innerHTML = params.content;
+      } 
+        
+      if (params.className){    
+         element.className = params.className;
       }
 
-      element.className = params.className;
-
-      
+      if (params.content){
+         element.innerHTML = params.content;
+      }
 
       if (params.parentElement){
       	 params.parentElement.appendChild(element);
       	}
-        return element;
-	}
-}
 
+        return element;
+	     },
+        generateQuestions: function(guestionsAmount, answersAmount) { 
+
+        for (var i = 0; i < guestionsAmount; i++) {
+
+          this.createElement({
+            tagName: 'h2',
+            content: 'Вопрос №' + (i + 1),  
+            parentElement: body
+        });
+
+         for (var j = 0; j < answersAmount; j++) {
+            this.createElement({
+            tagName: 'label',
+            content: '<input type="checkbox" /> Вариант ответа №' + (j + 1),
+            parentElement: body
+        });
+       }
+    };
+  }
+}
 
 
 var body = document.querySelector('body');
@@ -29,10 +47,16 @@ var body = document.querySelector('body');
 
 app.createElement({
    tagName: 'h1',
-   className: 'title',
    content: 'Тест по програмированию',
    parent: body
 });
+
+var form = app.createElement({
+   tagName: 'form',
+   parentElement: body
+});
+
+app.generateQuestions(3,3);
 
 app.createElement({
    tagName: 'input',
@@ -41,6 +65,5 @@ app.createElement({
    content: 'Проверить мои результаты',
    parentElement: body
 });
-
 
 
